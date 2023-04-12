63691bd092c243051d687f1efad6e94dfe3fa729
Ivan Amoshyi <amoshyiivan@gmail.com>
2023-04-12 18:50:38 +0300

    Додав можливість перевіряти введене слово на правильність

---
+++ src/components/Form.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import './Form.css';
+
+export default function Form({ ukrainian, showToggle }) {
+  const [inputResult, setInputResult] = useState('');
+  const [isWrong, setIsWrong] = useState(false);
+	const [showCorrect, setShowCorrect] = useState(false);
+
+  const handleChange = e => {
+    setInputResult(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (inputResult.toLowerCase() === ukrainian) {
+      setIsWrong(false);
+      setShowCorrect(prev => !prev);
+    } else {
+      setIsWrong(true);
+    }
+    setInputResult('');
+  };
+
+  return (
+    <>
+      <form
+        className='form'
+        autoComplete="off"
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          error={isWrong}
+          className="error"
+          id="outlined-basic"
+          variant="outlined"
+          size="small"
+          value={inputResult}
+          onChange={handleChange}
+        />
+        <p className={`correctText ${showCorrect ? 'isShown' : ' '}`}>
+          Correct!
+        </p>
+      </form>
+    </>
+  );
+}
--- src/components/Set.jsx
+++ src/components/Set.jsx
@@ -1,25 +1,26 @@
 import words from "../words.json";
-import Form from "./Input";
+import Form from "./Form";
 import './Set.css';
 
 export default function Set() {
 
-
 	return (
-		<div className="container">
-			<h1>Success</h1>
-			<ul className="wordsContainer" style={{listStyle: 'none', padding: 0}}>
+    <div className="container">
+      <h1 style={{ textAlign: 'center' }}>Success</h1>
+      <ul className="wordsContainer" style={{ listStyle: 'none', padding: 0 }}>
         {words.success.map(({ id, english, ukrainian }) => {
           return (
-						<li key={id}>
-							<div className="wordCard">
-								<p>{english}</p>
-								<Form ukrainian={ukrainian} />
-							</div>
-						</li>
-					);
+            <li key={id}>
+              <div className="wordCard">
+                <p style={{ textAlign: 'center' }}>{english}</p>
+                <Form
+                  ukrainian={ukrainian}
+                />
+              </div>
+            </li>
+          );
         })}
-			</ul>
-		</div>
-	);
+      </ul>
+    </div>
+  );
 }
